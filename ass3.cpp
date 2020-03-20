f4b5bba87616ebcf2adcc8fc0d3bdad566ac4202
maheshbansod <coolkevin011@gmail.com>
2020-03-20 18:12:56 +0530

    added command line option for disablling verbose output

--- ass3.cpp
+++ ass3.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cstring>
 #include "EquationSystem.hpp"
 #include "Matrix.hpp"
 
 using namespace std;
 
-int main() {
+int main(int argc, char **argv) {
     double coeff[]={10,-1,2,0, -1,11,-1,3, 2,-1,10,-1, 0,3,-1,8};
     double consts[]={6,25,-11,15};
     Matrix<double> a(coeff, 4, 4);
@@ -12,12 +13,24 @@ int main() {
     /**Exact solution of the above system is (1,2,-1,1). Taken from wikipedia. **/
     EquationSystem<double> es(a, b);
 
+    bool verbose = true;
+    if(argc>1) {
+        if(strcmp(argv[1],"n")==0) verbose = false;
+        else {
+            std::cout << "Invalid command line argument. Pass `n` to set verbose to false, pass nothing to set it to true.\nsetting verbose to false\n";
+            verbose = false;
+        }
+    }
+
     cout << "The given system is:\n"<<es;
     
     cout << "Performing GaussJacobi.\n";
-    Matrix<double> solution = es.gaussJacobi(true);
+    Matrix<double> solution = es.gaussJacobi(verbose);
 
-    cout << "Solution set:\n"<<solution;
+    cout << "-----\nSolution set:\n"<<solution<<"-----\n";
+
+    if(argc == 1)
+        std::cout << "You can pass the command line argument `n` to remove the detailed messages of the gauss jacobi algorithm.\n";
 
     return 0;
 }
